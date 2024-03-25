deeb886c3b37e197adc27bec91dd11bacde81ff4
Toshihiro Matsuo <tm.19950223.tm@gmail.com>
2024-03-25 20:12:03 +0900

    do-natu

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 from PIL import Image
 import os
 import config
+import matplotlib.pyplot as plt
+import numpy as np
 
 # FastAPIエンドポイント
 url = config.FASTAPI_URL
@@ -26,7 +28,8 @@ st.title('NutriVision')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    # 画像表示幅を500ピクセルに設定
+    st.image(image, caption='Uploaded Image.', width=500)
     
     # ファイルポインタをファイルの先頭に戻す
     uploaded_file.seek(0)
@@ -40,19 +43,37 @@ if uploaded_file is not None:
     st.success(f"Classified as: {class_name}")
     st.subheader('Nutrition Information')
 
-    # 栄養情報を表形式で表示
-    # st.table(nutrients)
 
-    # または、より詳細な情報をカード形式で表示
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:
-        st.metric(label="Energy", value=f"{nutrients['Energy']} kcal")
-    with col2:
-        st.metric(label="Protein", value=f"{nutrients['Protein']} g")
-    with col3:
-        st.metric(label="Fat", value=f"{nutrients['Fat']} g")
-    with col4:
-        st.metric(label="Carbohydrates", value=f"{nutrients['Carbohydrate']} g")
+    # ドーナツグラフのデータ準備
+    labels = ['Protein', 'Fat', 'Carbohydrates']
+    sizes = [nutrients['Protein'], nutrients['Fat'], nutrients['Carbohydrate']]
+    colors = ['#ff9999','#66b3ff','#99ff99']
+    
+    # グラフスタイルの調整
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots()
+    wedges, texts = ax.pie(sizes, colors=colors, startangle=90, wedgeprops=dict(width=0.3))
 
+    # ラベルを線で示す（ドーナツグラフのラベルを非表示に）
+    kw = dict(arrowprops=dict(arrowstyle="-", color='white'), zorder=0, va="center")
+    for i, p in enumerate(wedges):
+        ang = (p.theta2 - p.theta1)/2. + p.theta1
+        y = np.sin(np.deg2rad(ang))
+        x = np.cos(np.deg2rad(ang))
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = f"angle,angleA=0,angleB={ang}"
+        kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        # ここで各栄養素の値に単位の"g"を追加
+        ax.annotate(f"{labels[i]}: {sizes[i]}g", xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
+
+    # 中心の円でドーナツグラフの穴を作成
+    centre_circle = plt.Circle((0,0),0.70,fc='black')
+    fig.gca().add_artist(centre_circle)
+
+    # エネルギー値を中心に表示
+    plt.text(0, 0, f"{nutrients['Energy']} kcal", ha='center', va='center', color='white')
+    
+    st.pyplot(fig)
     
 
